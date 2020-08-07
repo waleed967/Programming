faa70fd83c3c380b087575305ca96205c0ffb8dc
waleed967 <waleedqureshi90@yahoo.com>
2020-08-07 03:41:22 -0700

    Inheritance Code

---
+++ Inheritance/Inheritance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Calcultor
+    {
+        protected double num1;
+
+        public void Input()
+        {
+            Console.WriteLine("Enter First Num");
+
+            num1 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+    class CalculatorChild : Calcultor
+    {
+        private double num2;
+
+        public void InputChild()
+        {
+            Input();
+            Console.WriteLine("Enter Second Num");
+            num2 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Sub()
+        {
+
+            double sub = num1 - num2;
+            Console.WriteLine("The Substraction Is " +sub);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorChild ic = new CalculatorChild();
+
+            ic.InputChild();
+            ic.Sub();
+
+            Console.ReadLine();
+        }
+    }
+}
