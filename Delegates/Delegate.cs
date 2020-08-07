b329e4870afd534a398d0e3d09a015e1b2d117ed
waleed967 <waleedqureshi90@yahoo.com>
2020-08-06 23:57:00 -0700

    Delegates code

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public delegate int mul(int x, int y);
+namespace Delegates
+{
+    class Program
+    {
+        private static int mult(int a , int b)
+        {
+            return a * b;
+
+        }
+        static void Main(string[] args)
+        {
+            mul obj = new mul(mult);
+            int result = obj(3, 2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
