cd7dc99b2e9ea68fcc043941f832252b5a5529ff
waleed967 <waleedqureshi90@yahoo.com>
2020-08-07 00:05:26 -0700

    Static Method code

---
+++ Static Method/Static Method.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class Program
+    {
+
+        static void Display()
+        {
+            Console.WriteLine("What's Your Name");
+
+        }
+        static void Main(string[] args)
+        {
+            Display();
+            Console.ReadLine();
+
+            
+        }
+    }
+}
