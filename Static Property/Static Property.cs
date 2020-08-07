5c686fbfa0a26b666f2793e3f04fd9b05d9eef99
waleed967 <waleedqureshi90@yahoo.com>
2020-08-07 00:06:06 -0700

    Static Property

---
+++ Static Property/Static Property.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticProperty
+{
+    class University
+    {
+        private static string _university;
+        
+
+        public static string universityname{
+            get
+            {
+
+                return _university;
+
+            }
+
+
+
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+
+                    Console.WriteLine("Cannot Enter Null or Empty");
+                }
+                else
+                {
+                    _university = value;
+
+                }
+                _university = value;
+
+            } }
+
+        static void Main(string[] args)
+        {
+            University.universityname = "Numl";
+            Console.WriteLine("University is    " + University.universityname);
+            Console.ReadLine();
+        }
+    }
+}
