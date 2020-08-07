717c203b8538550d6ea0fd12f869af9e853bd094
waleed967 <waleedqureshi90@yahoo.com>
2020-08-07 01:12:53 -0700

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IEmployee
+    {
+
+        void show();
+    }
+
+    class IStudent : IEmployee
+    {
+        public void show()
+        {
+            Console.WriteLine("Getting Data From interface");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IStudent st = new IStudent();
+
+            st.show();
+            Console.ReadLine();
+
+        }
+    }
+}
