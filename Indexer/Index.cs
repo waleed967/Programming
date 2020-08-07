4411d5416ff3bfb7ac19df0178c5f0c3a5b7015f
waleed967 <waleedqureshi90@yahoo.com>
2020-08-07 00:04:21 -0700

    Index Code

---
+++ Indexer/Index.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexxers
+{
+
+    public class person
+    {
+        string[] persons = { "Ali", "Akbar", "Ahmed" };
+        public string this[int index]
+        {
+            get
+            {
+                return persons[index];
+            }
+
+            set
+            {
+                persons[index] = value;
+
+
+            }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person obj = new person();
+
+            Console.WriteLine("First person {0}", obj[0]);
+            Console.ReadLine();
+
+        }
+    }
+}
