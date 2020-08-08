c8a556e4e798324535e2785cf2a86fc7f085d357
waleed967 <waleedqureshi90@yahoo.com>
2020-08-07 23:05:38 -0700

    Overriding Code

---
+++ Overriding/Overriding.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverRiding
+{
+    class Program
+    {
+        class Person
+        {
+            protected int _id;
+            protected string _name;
+
+            virtual public void Display()
+            {
+                Console.WriteLine("Id  " + _id);
+                Console.WriteLine("Name  " + _name);
+
+            }
+        }
+
+        class Student : Person
+        {
+            int _roll;
+            string _semester;
+
+            public Student(int _id , string _name , int _roll, string _semester)
+            {
+                base._id = _id;
+                base._name = _name;
+                this._roll = _roll;
+                this._semester = _semester;
+            }
+
+            override public void Display()
+            {
+                base.Display();
+                Console.WriteLine("Roll No " + _roll);
+                Console.WriteLine("Semester " + _semester);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student st = new Student(413, "Waleed", 1007, "8th");
+            st.Display();
+            Console.ReadLine();
+        }
+    }
+}
